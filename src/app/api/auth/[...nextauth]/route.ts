46f4a93511c14d95da7c09c6b8ab637453992310
build-with-paradox <contact@buildwithparadox.com>
2025-05-18 20:13:20 +0530

    fixed export issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -35,7 +35,7 @@ declare module "next-auth/jwt" {
   }
 }
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || "",
